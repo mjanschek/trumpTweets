eba2f3184fa9d3eebee1da17a159bc03a294b5da
Michael Janschek <michael.janschek@gmx.de>
2017-11-23 23:44:01 +0100

    add opencsv to spark-submit

--- src/main/bash/startTweetStream.sh
+++ src/main/bash/startTweetStream.sh
@@ -5,4 +5,6 @@ SPARKDIR=/home/garg/spark-2.2.0-bin-hadoop2.7/
 
 $SPARKDIR/sbin/start-all.sh
 
-$SPARKDIR/bin/spark-submit --packages org.apache.bahir:spark-streaming-twitter_2.11:2.2.0 $TWEETDIR/TrumpTweets-0.1.jar 
+$SPARKDIR/bin/spark-submit --packages=org.apache.bahir:spark-streaming-twitter_2.11:2.2.0,com.opencsv:opencsv:4.1 $TWEETDIR/TrumpTweets-0.1.jar 
+
+$SHELL
--- src/main/bash/startTweetUpdates.sh
+++ src/main/bash/startTweetUpdates.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TWEETDIR=/home/garg/tweets/
-SPARKDIR=/home/garg/spark-2.2.0-bin-hadoop2.7/
+TWEETDIR=/home/garg/tweets
+SPARKDIR=/home/garg/spark-2.2.0-bin-hadoop2.7
 
 python3 $TWEETDIR/updateTweets.py -l -d $TWEETDIR
+
+$SHELL
